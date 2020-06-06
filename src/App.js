8a2c98e4cd9e6f24f8a22382fbb57f78f8172a5c
antiquehj <hyunjooj@naver.com>
2020-06-06 16:07:51 +0900

    App content changed

--- src/App.js
+++ src/App.js
@@ -4,20 +4,15 @@ import "./App.scss";
 import Navbar from "./components/Navbar/Navbar";
 import TourList from "./components/TourList";
 
-
 class App extends Component {
   render() {
     return (
       <main>
         <Navbar />
-        <TourList/>
-        <h1>City Tour HJ!!!</h1>
-        <h1>나의 첫번째 리액트 홈페이지</h1>
+        <TourList />
       </main>
     );
   }
 }
 
 export default App;
-
-
